d2e6d23abede4d14b07e7ab9e405f1d6980cd0c6
kurohero1 <tky2402108@stu.o-hara.ac.jp>
2025-05-07 11:23:52 +0900

    Update TestListStudentDao.java

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -10,60 +10,46 @@ import java.util.List;
 import bean.Student;
 import bean.TestListStudent;
 
-public class TestListStudentDao extends Dao{
+public class TestListStudentDao extends Dao {
 
-	private String baseSql = "SELECT * FROM subject WHERE no=?";
+    // 使用していないSql
+    private String baseSql = "SELECT * FROM TestListStudent WHERE subjectCd=?";
 
-	private List<TestListStudent> postFilter(ResultSet rSet, Student student) throws Exception {
-		List<TestListStudent> list = new ArrayList<>();
-		try {
-			while (rSet.next()) {
-				TestListStudent testListStudent = new TestListStudent();
-				testListStudent.setSubjectName(rSet.getString("subjectName"));
-				testListStudent.setSubjectCd(rSet.getString("subjectCd"));
-				testListStudent.setNum(rSet.getInt("num"));
-				testListStudent.setPoint(rSet.getInt("point"));
-				list.add(testListStudent);
-			}
-		} catch (SQLException | NullPointerException e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
+    public List<TestListStudent> filter(Student student) throws Exception {
+        //使用しているSql
+        String baseSql = "SELECT t.subject_cd, s.name AS subject_name, t.no AS num, t.point " +
+                    "FROM test t " +
+                    "JOIN subject s ON t.subject_cd = s.cd AND t.school_cd = s.school_cd " +
+                    "WHERE t.student_no = ? AND t.school_cd = ? " +
+                    "ORDER BY t.subject_cd, t.no";
 
-	public List<TestListStudent> filter(Student student) throws Exception {
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(baseSql)) {
 
-        List<TestListStudent> list = new ArrayList<>();
+            ps.setString(1, student.getNo());
+            ps.setString(2, student.getSchool().getCd());
 
-        Connection connection = getConnection();
-
-        PreparedStatement statement = null;
-
-        ResultSet rSet = null;
-
-        try {
-
-            statement = connection.prepareStatement(baseSql + " ORDER BY cd ASC");
-
-            statement.setString(1, student.getNo());
-
-            rSet = statement.executeQuery();
-
-            list = postFilter(rSet, student);
-
-        } finally {
-
-            if (rSet != null) try { rSet.close(); } catch (SQLException e) { throw e; }
-
-            if (statement != null) try { statement.close(); } catch (SQLException e) { throw e; }
-
-            if (connection != null) try { connection.close(); } catch (SQLException e) { throw e; }
-
+            try (ResultSet rs = ps.executeQuery()) {
+                return postFilter(rs, student); // postFilterを使って結果を処理する
+            }
         }
-
-        return list;
-
     }
 
-
+    //
+    private List<TestListStudent> postFilter(ResultSet rSet, Student student) throws Exception {
+        List<TestListStudent> list = new ArrayList<>();
+        try {
+            while (rSet.next()) {
+                TestListStudent test = new TestListStudent();
+                test.setSubjectCd(rSet.getString("subject_cd"));
+                test.setSubjectName(rSet.getString("subject_name"));
+                test.setNum(rSet.getInt("num"));
+                test.setPoint(rSet.getInt("point"));
+                list.add(test);
+            }
+        } catch (SQLException e) {
+            throw new Exception("成績データの処理中にエラーが発生しました", e);
+        }
+        return list;
+    }
 }
