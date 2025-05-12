47baba4fcbc290ac3030c0582442a467eef986b7
kurohero1 <tky2402108@stu.o-hara.ac.jp>
2025-05-12 11:53:10 +0900

    Update ClassCreateAction.java

--- src/scoremanager/main/ClassCreateAction.java
+++ src/scoremanager/main/ClassCreateAction.java
@@ -10,6 +10,7 @@ import bean.ClassNum;
 import bean.School;
 import bean.Teacher;
 import dao.ClassNumDao;
+import dao.SchoolDao;
 import tool.Action;
 
 public class ClassCreateAction extends Action {
@@ -21,35 +22,49 @@ public class ClassCreateAction extends Action {
 
         int line = 0;
         try {
-            String classNum = "";
-
-            ClassNumDao cNumDao = new ClassNumDao();
-
-            classNum = req.getParameter("class_num");
-
-            List<String> classNumSet = cNumDao.filter(teacher.getSchool());
-
-            ClassNum s = new ClassNum();
-            ClassNumDao dao = new ClassNumDao();
+            // 現在ログインしている教師の学校情報を取得
             School schoolCd = teacher.getSchool();
 
-            s.setSchool(schoolCd);
-            s.setClass_num(classNum);
+            // 学校情報を全て取得する
+            SchoolDao schoolDao = new SchoolDao();
+            List<School> schoolList = schoolDao.getAllSchools(); // すべての学校情報を取得
 
-            req.setAttribute("classNumSet", classNumSet);
-            req.setAttribute("schoolCd", schoolCd);
+            // 現在の学校のクラス番号リストを取得
+            ClassNumDao cNumDao = new ClassNumDao();
+            List<String> classNumSet = cNumDao.filter(schoolCd); // 現在の学校に関連するクラス番号を取得
 
+            // リクエスト属性に学校リスト、クラス番号リスト、学校コードを設定
+            req.setAttribute("schoolList", schoolList);  // 学校情報
+            req.setAttribute("classNumSet", classNumSet);  // 現在の学校のクラス情報
+            req.setAttribute("schoolCd", schoolCd);  // 現在の学校のコード
+
+            // 新しいクラス番号のための ClassNum インスタンスを作成
             ClassNum cc = new ClassNum();
-            cc.setSchool(teacher.getSchool());
-            req.setAttribute("classNum", cc);
+            cc.setSchool(schoolCd);
+            req.setAttribute("classNum", cc);  // 新しいクラスインスタンスをリクエストに設定
 
-            line = dao.insertClass(s);
+            // フォームから送信されたクラス番号を取得
+            String classNum = req.getParameter("class_num");
+
+            // クラス番号が空でない場合、新しいクラスを登録
+            if (classNum != null && !classNum.isEmpty()) {
+                ClassNum s = new ClassNum();
+                s.setSchool(schoolCd);
+                s.setClass_num(classNum);
+
+                // 新しいクラス情報をデータベースに挿入
+                line = cNumDao.insertClass(s);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // クラス情報登録結果をリクエストに設定
         req.setAttribute("line", line);
+        // 登録ページにフォワード
         req.getRequestDispatcher("class_create.jsp").forward(req, res);
     }
 }
+
 
